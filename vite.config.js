38a04203ba78a45318a1347c7d2c94d2f99d0713
Salesf78 <34050486+Salesf78@users.noreply.github.com>
2026-09-07 19:07:33 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ export default defineConfig({
 
   server: {
     proxy: {
-      '/api': {
+       // 1. Catch /data-api requests coming from your React code on port 5173
+      '/data-api': {
         target: 'http://localhost:5000',
-        changeOrigin: true
+        changeOrigin: true,
+        // 2. Strip out "/data-api" so port 5000 gets exactly what it wants
+        rewrite: (path) => path.replace(/^\/data-api/, '') 
       }
     }
   }
